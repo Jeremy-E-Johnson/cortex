b84c88f114c5f26f1e59e1e2712c4e78c6358e8b
Jeremy-E-Johnson <Jeremy.Johnson14@ncf.edu>
2016-06-20 16:56:45 -0600

    Added _vis(). Appears to be functional.

--- cortex/demos/demos_basic/rnn_europarl.py
+++ cortex/demos/demos_basic/rnn_europarl.py
@@ -6,7 +6,9 @@ Try with cortex-run 'rnn_europarl.py <optional .yaml>'
 
 from collections import OrderedDict
 import theano
+import numpy as np
 from cortex.models.rnn import SimpleRNN
+from cortex.utils import intX, floatX
 from cortex.datasets import resolve as resolve_dataset
 
 
@@ -14,20 +16,21 @@ from cortex.datasets import resolve as resolve_dataset
 _learning_args = dict(
     learning_rate=0.01,
     learning_rate_scheduler=None,
-    optimizer='sgd',
+    optimizer='rmsprop',
     optimizer_args=dict(),
     epochs=100,
-    valid_key='error',
+    valid_key='-sum log p(x | y)',
     valid_sign='+',
     excludes=[]
 )
 
 _dataset_args = dict(
-    train_batch_size=100,
-    valid_batch_size=100,
+    train_batch_size=10,
+    valid_batch_size=10,
+    debug=False,
     dataset='europarl',
     distribution='multinomial',
-    source='$data/basic/'
+    source='$data/basic/europarl'
 )
 
 _model_args = dict(
@@ -36,7 +39,7 @@ _model_args = dict(
 )
 
 simple_rnn_args = dict(
-    dim_h=100,
+    dim_h=1000,
     input_layer='europarl',
     output='label',
     dropout=None
@@ -44,13 +47,19 @@ simple_rnn_args = dict(
 
 extra_arg_keys = ['simple_rnn_args']
 
+#theano.config.compute_test_value = 'warn'
+
+#theano.config.exception_verbosity = 'high'
+
+#theano.config.optimizer = 'None'
+
 
 def _build(module):
     models = OrderedDict()
     dataset = module.dataset
     simple_rnn_args = module.simple_rnn_args
-    dim_in = dataset.dims[dataset.name]
-    dim_out = dataset.dims['label']
+    dim_in = dataset.dimsall[dataset.name][2]
+    dim_out = dataset.dimsall[dataset.name][2]
     distribution = dataset.distributions[simple_rnn_args['output']]
 
     model = SimpleRNN.factory(dim_in=dim_in, dim_out=dim_out, distribution=distribution, **simple_rnn_args)
@@ -62,20 +71,17 @@ def _build(module):
 def _cost(module):
     models = module.models
 
-    X = module.inputs[module.dataset.name]
+    X = module.inputs[module.dataset.name].transpose(1, 0, 2)
     used_inputs = [module.dataset.name]
 
     model = models['rnn']
-    outputs = model(X)
+    outputs, preact, updates = model(X)
 
     results = OrderedDict()
-    p = outputs[0]['p']
-    base_cost = model.neg_log_prob(X[1:], p[:-1]).sum(axis=0)
-    print base_cost, '####'
-    print model.neg_log_prob(X[1:], p[:-1]), '#####'
+    p = outputs['p']
+    base_cost = model.neg_log_prob(X[1:], p[:-1]).sum(0).mean()
     cost = base_cost
 
-    updates = theano.OrderedUpdates()
     constants = []
 
     l2_decay = module.l2_decay
@@ -90,4 +96,25 @@ def _cost(module):
     results['-sum log p(x | y)'] = base_cost
     results['cost'] = cost
 
-    return used_inputs, results, updates, constants, outputs[0]
+    return used_inputs, results, updates, constants, outputs
+
+
+def _viz(module, outputs, results, n_samples=10, n_steps=10):
+    out_path = module.out_path
+    out_path = None #### For testing purposes
+    n_tokens = int(module.dataset.dimsall[module.dataset.name][2])
+
+    pvals = np.zeros((n_samples, n_tokens)) + 1./float(n_tokens)
+    x0 = module.models['rnn'].trng.multinomial(pvals=pvals, dtype=floatX)
+
+    outputs, updates = module.models['rnn'].sample(x0=x0, n_steps=n_steps)
+
+    updates = theano.OrderedUpdates(updates)
+
+    f_vis = theano.function([], outputs['x'], updates=updates)
+
+    def f_analysis():
+        out = f_vis()
+        return module.dataset.save_images(out, out_path=out_path)
+
+    return f_analysis
