77ebe3a324aac1d80d4305d9fc073148e93ee641
Jeremy-E-Johnson <Jeremy.Johnson14@ncf.edu>
2016-07-22 12:55:59 -0600

    Added proper test for __call__ function that compares it to numpy calculated values.

--- cortex/models/tests/test_pyramid_rnn.py
+++ cortex/models/tests/test_pyramid_rnn.py
@@ -51,4 +51,30 @@ def test_call(pyramid=None, dim_in=1, dim_h=17, width=13):
 
     y = pyramid(x)
 
-    f = theano.function([], y[0]['p'])
+    f = theano.function([], y[0]['z'])
+
+    # Now calculate what f should be using Numpy.
+
+    outs =[]
+    for k in range(0, 4):  # Iterate through directions
+        x = np.rot90(x.swapaxes(1, 2)).swapaxes(1, 2)  # Rotate input
+
+        dir_input = x[:(width + 1)/2, :, :, None]
+        dir_input = pyramid.input_net.params['b0'] + np.dot(dir_input, pyramid.input_net.params['W0'])
+
+        h = np.zeros((x.shape[1], width, dim_h))
+        Ur = pyramid.params['Ur0']
+
+        for layer in dir_input:  # Iterate through height of pyramid
+            h_t = np.concatenate((h, np.roll(h, 1, 2), np.roll(h, -1, 2)), 2)
+            preact = layer + np.dot(h_t, Ur)
+            h = np.tanh(preact)
+
+        outs.append(h[:, (width + 1)/2, :])  # Remember output for direction
+
+    output = pyramid.output_net.params['b0'] + np.dot(sum(outs), pyramid.output_net.params['W0'])  # Sum over direction\
+    # and apply output network.
+
+    # Test for equality.
+
+    np.testing.assert_almost_equal(output, f())  # Check if they match.
